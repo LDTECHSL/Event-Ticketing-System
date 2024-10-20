628402ed16c89b82bbd6d0b905a970aa2390652a
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2024-10-20 17:17:52 +0530

    commit

---
+++ ticket-system-backend/src/main/java/com/ticket/system/ticketsystembackend/TicketSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.system.ticketsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSystemBackendApplication.class, args);
+	}
+
+}
---
+++ ticket-system-backend/src/test/java/com/ticket/system/ticketsystembackend/TicketSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.system.ticketsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
